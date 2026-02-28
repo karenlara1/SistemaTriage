47f7badc1dffa72c1bc239a90a2f7166c6b5f9b5
karenlara1 <karena.larab@uqvirtual.edu.co>
2026-02-27 19:50:35 -0500

    Primer Commit

---
+++ src/main/java/co/edu/uniquindio/sistematriage/SistemaTriageApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sistematriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaTriageApplication.class, args);
+    }
+
+}
