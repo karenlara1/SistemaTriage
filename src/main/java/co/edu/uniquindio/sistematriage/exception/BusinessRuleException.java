dfd01c8f5e5a2dbce48b72fb72442621e18706f0
Jose Luis Rivadeneira Hernandez <jlrivadeneirah@uqvirtual.edu.co>
2026-03-25 23:50:12 -0500

    Exception
    BusinessRuleException
    ResourceNotFoundException

---
+++ src/main/java/co/edu/uniquindio/sistematriage/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.sistematriage.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/sistematriage/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.sistematriage.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
